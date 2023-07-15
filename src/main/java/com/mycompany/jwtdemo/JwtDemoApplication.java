eb3b30a2b613dea7c9f5676fb0f6618845c90fcb
jakane-salah <salahedinejakane@gmail.com>
2023-07-15 12:34:44 +0100

    test

--- src/main/java/com/mycompany/jwtdemo/JwtDemoApplication.java
+++ src/main/java/com/mycompany/jwtdemo/JwtDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JwtDemoApplication {
+	/* test */
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtDemoApplication.class, args);
